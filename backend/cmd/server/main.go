26155f3d5de731acc11332d5475b8e55b6ed7c13
MindlessMuse666 <mindlessmuse.666@gmail.com>
2025-09-16 21:31:25 +0300

    fix(be): http routes

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/MindlessMuse666/ru-jp-dict/backend/internal/database"
 	"github.com/MindlessMuse666/ru-jp-dict/backend/internal/handlers"
@@ -17,13 +19,26 @@ func main() {
 	}
 	defer db.Close()
 
+	// Абсолютный путь к корню
+	basePath := getRootPath()
+
 	// Создание репозитория
 	repo := repository.NewVocabularyRepo(db)
 
 	// Настройка HTTP-роутинга
-	router := handlers.SetupRouter(repo)
+	router := handlers.SetupRouter(repo, basePath)
 
 	// Запуск сервера
-	log.Println("запуск сервера на http://localhost:8080")
+	log.Println("start server on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+// Получает абсолютный путь к корню проекта
+func getRootPath() string {
+	execPath, err := os.Executable()
+	if err != nil {
+		log.Fatal("failed to get executable path: ", err)
+	}
+
+	return filepath.Dir(execPath)
 }
